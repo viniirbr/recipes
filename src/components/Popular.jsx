73202a0ec0d8b9f22d3b188242dd7e03243cb402
Vinicius Ribeiro <vayne99@gmail.com>
2022-03-16 15:42:17 -0300

    Get data from API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 
 function Popular() {
-  
-  const getPopular = async () => {
-      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=9`);
-      const data = await api.json();
-      console.log(data)
-  }
+
+    useEffect(() => {
+        getPopular()
+    }, [])
+
+    const [popular, setPopular] = useState([])
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes)
+    }
     return (
-    <div>Popular</div>
-  )
+        <div>
+            {popular.map((recipe) => {
+                return(
+                    <div key={recipe.id}>
+                        <img src={recipe.image}/>
+                    </div>
+                );
+            })}
+        </div>
+    )
 }
 
 export default Popular
