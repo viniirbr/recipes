2424ed534a95943dbbee066cd897a2c3a9940ab2
Vinicius Ribeiro <vayne99@gmail.com>
2022-03-16 15:15:37 -0300

    Initial commit

---
+++ src/components/Popular.jsx
@@ -0,0 +1,14 @@
+
+function Popular() {
+  
+  const getPopular = async () => {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=9`);
+      const data = await api.json();
+      console.log(data)
+  }
+    return (
+    <div>Popular</div>
+  )
+}
+
+export default Popular
